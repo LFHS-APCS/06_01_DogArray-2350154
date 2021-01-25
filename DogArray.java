ddc5af9dfc034bd360d25bb9a59a3dde9e9964fc
2350154 <69920985+2350154@users.noreply.github.com>
2021-01-25 11:15:33 -0600

    Update DogArray.java

--- DogArray.java
+++ DogArray.java
@@ -4,16 +4,28 @@ public class DogArray
     /**
      *  Returns the index of the youngest dog in the given array
      */
-    public static int getYoungestIndex(Dog[] dogs)
+    public int getYoungestIndex(Dog[] dogs)
+{
+    int index = 0; // initialize index, pick first one 
+    for (int i = 1; i < dogs.length; i++) // loop through all
     {
-        return -1;
+      if (dogs[i].getAge() < dogs[index].getAge()) // compare ages
+        index = i;
     }
+    return index; // return requested answer... read the question!
+}
 
     /**
      *  Returns the name of the youngest dog in the given array
      */
     public static String getYoungestName(Dog[] dogs)
     {
-        return "";
-    }
+      int index = 0; 
+      for (int i = 1; i < dogs.length; i++)
+      {
+        if (dogs[i].getAge() < dogs[index].getAge()) 
+          index = i;
+      }
+      return dogs[index];
+      }
 }
